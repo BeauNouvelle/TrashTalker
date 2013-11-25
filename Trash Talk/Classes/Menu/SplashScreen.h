6058933f5232e3d3a807c2bcc201828e8f23b374
Beau <beau@sharpagency.com.au>
2013-11-25 15:08:27 +1100

    Readme Added

--- Trash Talk/Classes/Menu/SplashScreen.h
+++ Trash Talk/Classes/Menu/SplashScreen.h
@@ -1,13 +1,13 @@
 //
-//  MyScene.h
+//  SplashScreen.h
 //  Trash Talk
 //
-
+//  Created by Sharp Agency on 25/11/2013.
 //  Copyright (c) 2013 Sharp Agency. All rights reserved.
 //
 
 #import <SpriteKit/SpriteKit.h>
 
-@interface MyScene : SKScene
+@interface SplashScreen : SKScene
 
 @end
---
+++ Trash Talk/Classes/Menu/SplashScreen.m
@@ -0,0 +1,13 @@
+//
+//  SplashScreen.m
+//  Trash Talk
+//
+//  Created by Sharp Agency on 25/11/2013.
+//  Copyright (c) 2013 Sharp Agency. All rights reserved.
+//
+
+#import "SplashScreen.h"
+
+@implementation SplashScreen
+
+@end
